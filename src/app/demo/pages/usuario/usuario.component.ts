6964c55214d5192f030c875c5fd3d38778866f7c
gmora-asd <lmora@grupoasd.com.co>
2025-10-04 12:40:12 -0500

    feat: guardar usuario nuevo

--- src/app/demo/pages/usuario/usuario.component.ts
+++ src/app/demo/pages/usuario/usuario.component.ts
@@ -7,14 +7,26 @@ import Swal from 'sweetalert2';
 // Importa los objetos necesarios de Bootstrap
 import Modal from 'bootstrap/js/dist/modal';
 
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+  AbstractControl,
+  FormsModule,
+  ReactiveFormsModule,
+  ValidationErrors
+} from '@angular/forms';
+import { delay, map, Observable, of } from 'rxjs';
+
 @Component({
   selector: 'app-usuario',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './usuario.component.html',
   styleUrl: './usuario.component.scss'
 })
 export class UsuarioComponent {
-
+  mostrarPassword: boolean = false;
   modalInstance: Modal | null = null;
   modoFormulario: string = '';
   usuarios: Usuario[] = [];
@@ -22,15 +34,47 @@ export class UsuarioComponent {
   titleBoton: string = '';
   usuarioSelected: Usuario;
 
-  constructor(private readonly usuarioService: UsuarioService) {
-    console.log('Usuarios Components Cargado');
+  form: FormGroup = new FormGroup({
+    username: new FormControl(''),
+    pass: new FormControl(''),
+    rol: new FormControl(''),
+    activo: new FormControl('')
+  });
+
+  constructor(
+    private readonly usuarioService: UsuarioService,
+    private readonly formBuilder: FormBuilder
+  ) {
     this.listarUsuarios();
+    this.inicializarFormulario();
+  }
+
+  inicializarFormulario() {
+    this.form = this.formBuilder.group({
+      username: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(10)]],
+      pass: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(12)], [this.passwordAsyncValidator]],
+      rol: ['', [Validators.required]],
+      activo: [true]
+    });
+  }
+
+  passwordAsyncValidator(control: AbstractControl): Observable<ValidationErrors | null> {
+    const contrasenasProhibidas = ['123456', 'password', 'admin'];
+
+    return of(contrasenasProhibidas.includes(control.value)).pipe(
+      delay(800), // simulamos llamada a servidor
+      map((invalida) => (invalida ? { passwordProhibida: true } : null))
+    );
+  }
+
+  get f(): { [key: string]: AbstractControl } {
+    return this.form.controls;
   }
 
   listarUsuarios() {
     console.log('Listando Usuarios..');
     this.usuarioService.listarUsuarios().subscribe({
-      next: (data) => {        
+      next: (data) => {
         this.usuarios = data;
         console.log(this.usuarios);
       },
@@ -60,20 +104,54 @@ export class UsuarioComponent {
 
   abrirNuevoUsuario() {
     this.usuarioSelected = new Usuario();
-    // Cargamos los datos del usuario seleccionado en el formulario
-    
+    this.limpiarFormulario();
     // Dejamos el formulario en blanco
     this.openModal('C');
   }
 
   abrirEditarUsuario(usuario: Usuario) {
+    this.limpiarFormulario();
     this.usuarioSelected = usuario;
     this.openModal('E');
   }
 
   guardarUsuario() {
-    Swal.fire("Titulo", "Guardando Usuario... espere por favor", "info");
+    if (this.modoFormulario === 'C') {
+      this.form.get('activo')?.setValue(true);
+    }
+    if (this.form.invalid) {
+      // Manejar el formulario inválido
+      Swal.fire('Error', 'Por favor, corrige los errores en el formulario.', 'error');
+      return;
+    }
+
+    if (this.modoFormulario === 'C') {
+      // Modo Creación
+      this.usuarioService.guardarUsuario(this.form.getRawValue()).subscribe({
+        next: (data) => {
+          console.log(data);
+          if (data.status === 200) {
+            Swal.fire('Éxito', data.mensaje, 'success');
+            this.closeModal();
+            this.listarUsuarios();
+          } else {
+            Swal.fire('Error', data.mensaje, 'error');
+          }
+        },
+        error: (error) => {
+          console.error('Error al guardar usuario', error);
+          Swal.fire('Error', error.error.message, 'error');
+        }
+      });
+    } else {
+      // Modo Edición
+    }
   }
 
+limpiarFormulario() {
+  this.form.reset();
+  this.form.markAsPristine();
+  this.form.markAsUntouched();
+}
 
 }
