5a4e53cc100b60886068b0abb5f8c0a50789c13d
Camila.Salazar <lcsalazar08@gmail.com>
2025-10-18 23:16:37 -0500

    creacion de servicios

--- src/app/demo/pages/citas/models/citas.ts
+++
@@ -1,9 +0,0 @@
-export interface Citas {
-  id: number;
-  paciente_id: number;
-  medico_id: number;
-  motivo: string;
-  fecha_hora: string;
-  estado: string;
-}
-
--- src/app/demo/pages/formulas/models/formulas.ts
+++
@@ -1,8 +0,0 @@
-export interface Formulas {
-  id?: number;
-  cita_id: string;
-  medicamento_id: string;
-  dosis?: string;
-  indicaciones?: string;
-  fecha_creacion_registro?: string;
-}
